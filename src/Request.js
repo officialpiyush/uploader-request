ab28fe5eee3bd4e08ea1052744adafa37eb9ed59
Piyush Bhangale <bhangalepiyush@gmail.com>
2018-12-05 22:44:16 +0530

    Lol

---
+++ src/Request.js
@@ -0,0 +1,10 @@
+const fetch = require('node-fetch');
+const FormData = require('form-data');
+
+class Request{
+    constructor(options){
+        this.options = options;
+    };
+}
+
+module.exports = Request;
